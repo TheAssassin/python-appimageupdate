f231da5821e74f9f023a8892881a4030a9220047
TheAssassin <theassassin@assassinate-you.net>
2018-04-02 15:18:26 +0200

    Move build code to separate script
    This way, it can be tested locally.

---
+++ travis/travis-build.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+set -x
+set -e
+
+# use RAM disk if possible
+if [ -d /dev/shm ]; then
+    TEMP_BASE=/dev/shm
+else
+    TEMP_BASE=/tmp
+fi
+
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" AppImageUpdate-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+
+trap cleanup EXIT
+
+# store repo root as variable
+REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+OLD_CWD=$(readlink -f .)
+
+pushd "$BUILD_DIR"
+
+# installing boost::python from source is annoying
+# therefore using a miniconda environment for testing
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b -p conda-env/usr -f
+
+. conda-env/usr/bin/activate
+
+conda install -y -c statiskit libboost_python-dev libboost_python
+
+env | grep CONDA
+
+python "$REPO_ROOT"/setup.py develop
+py.test "$REPO_ROOT"/tests/
